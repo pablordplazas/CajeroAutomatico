b2a759e98da9cee54c8e614ddb44f7d3aea62dc1
pablordplazas <pablordplazas@gmail.com>
2025-09-19 15:08:15 -0500

    terminando main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,36 @@
-
 public class Main {
     public static void main(String[] args) {
-        cajeroAutomatico Cuenta1 = new cajeroAutomatico();
+        // Crear un objeto con el constructor con parámetros
+        cajeroAutomatico cuenta1 = new cajeroAutomatico(123456, 987654321, 1000.0);
 
+        // Mostrar el objeto con toString
+        System.out.println(cuenta1);
 
+        // Ingresar dinero
+        cuenta1.ingresar(500);
 
+        // Retirar dinero
+        cuenta1.retirar(300);
+
+        // Intentar retirar más de lo que hay
+        cuenta1.retirar(2000);
+
+        // Hacer extracción rápida
+        cuenta1.extraccionRapida();
+
+        // Consultar saldo
+        cuenta1.consultarSaldo();
+
+        // Crear un objeto con el constructor vacío y setear datos
+        cajeroAutomatico cuenta2 = new cajeroAutomatico();
+        cuenta2.setNumerodecuenta(654321);
+        cuenta2.setCeduladelcliente(123456789);
+        cuenta2.setSaldoactual(200);
+
+        // Probar métodos
+        cuenta2.consultarSaldo();
+        cuenta2.ingresar(100);
+        cuenta2.extraccionRapida();
+        cuenta2.consultarSaldo();
     }
 }
